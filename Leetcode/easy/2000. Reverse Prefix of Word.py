e143e2e309abc109da67b0ec7da6560b11079e4f
Alexander Kapturov <ultraluxe25@mail.ru>
2024-05-01 07:09:57 +0300

    Updated my solution after reading forum at LeetCode

--- Leetcode/easy/2000. Reverse Prefix of Word.py
+++ Leetcode/easy/2000. Reverse Prefix of Word.py
@@ -43,8 +43,12 @@ ch is a lowercase English letter.
 
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        if ch not in word:
-            return word
+        # if ch not in word:
+        #     return word
 
-        index = word.index(ch)
-        return word[:index + 1][::-1] + word[index + 1:]
+        char = word.find(ch)
+        if char != -1:
+            index = word.index(ch)
+            return word[:index + 1][::-1] + word[index + 1:]
+            
+        return word
