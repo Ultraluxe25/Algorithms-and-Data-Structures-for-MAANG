f7826daa92d4670bd1e1d2e01be70085aa9e2868
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-02 11:20:55 +0300

    Added 3rd solution for task 4

--- Leetcode/Hard problems/4._Median_of_Two_Sorted_Arrays.py
+++ Leetcode/Hard problems/4._Median_of_Two_Sorted_Arrays.py
@@ -26,6 +26,7 @@ Constraints:
 nums1.length == m
 """
 
+# Solution 1 (Brute force)
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         numbers = sorted(nums1 + nums2)
@@ -40,9 +41,35 @@ class Solution:
         return result
     
 
-# import numpy as np
+# Solution 2 (NumPy)
+import numpy as np
 
-# class Solution:
-#     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-#         return np.median(nums1 + nums2)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        return np.median(nums1 + nums2)
+    
+
+# Solution 3 (Two pointers)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        result = []
+        i = 0  # Two pointers
+        j = 0
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+
+        result += nums1[i:]
+        result += nums2[j:]
+        
+        n = len(result)
+        if n % 2 == 1:
+            return result[n // 2]
+        else:
+            return (result[n // 2 - 1] + result[n // 2]) / 2
     
