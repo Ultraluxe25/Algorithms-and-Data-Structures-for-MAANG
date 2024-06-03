0d3342b9a01af0ea4a67b2ed0d1838de0e8f7502
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-03 16:54:25 +0300

    Solved task 2486

---
+++ Leetcode/Medium problems/2486. Append Characters to String to Make Subsequence.py
@@ -0,0 +1,60 @@
+"""
+2486. Append Characters to String to Make Subsequence
+Solved 03.06.2024
+Medium
+
+Hint
+You are given two strings s and t consisting of only lowercase English letters.
+
+Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing 
+the order of the remaining characters.
+
+ 
+Example 1:
+
+Input: s = "coaching", t = "coding"
+Output: 4
+Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+Now, t is a subsequence of s ("coachingding").
+It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+
+
+Example 2:
+
+Input: s = "abcde", t = "a"
+Output: 0
+Explanation: t is already a subsequence of s ("abcde").
+
+
+Example 3:
+
+Input: s = "z", t = "abcde"
+Output: 5
+Explanation: Append the characters "abcde" to the end of s so that s = "zabcde".
+Now, t is a subsequence of s ("zabcde").
+It can be shown that appending any 4 characters to the end of s will never make t a subsequence.
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 105
+s and t consist only of lowercase English letters.
+"""
+
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        n = len(t)
+        prefix = 0
+        i = 0  # Two pointers
+        j = 0
+
+        while i < len(s) and j < n:
+            if s[i] == t[j]:
+                prefix += 1
+                j += 1
+            i += 1
+
+        return n - prefix
+        
