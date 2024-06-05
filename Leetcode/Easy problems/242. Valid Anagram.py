205b306f4cd4a50850d4ce8ec2fe7fc3564915d3
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-05 14:20:53 +0300

    Solved task 242

---
+++ Leetcode/Easy problems/242. Valid Anagram.py
@@ -0,0 +1,39 @@
+"""
+242. Valid Anagram
+Solved 05.06.2024
+Easy
+Topics
+Companies
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+        
