bea5dc94bd2292493441318cae21f53242e40080
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-05 14:26:43 +0300

    Solved task 349

---
+++ Leetcode/Easy problems/349. Intersection of Two Arrays.py
@@ -0,0 +1,32 @@
+"""
+349. Intersection of Two Arrays
+Solved 05.06.2024
+Easy
+
+Given two integer arrays nums1 and nums2, return an array of their 
+intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+"""
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1) & set(nums2)
+        
