a7ce87dc2afc0cc8c992e509f6a7a745d50c32a2
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-05 15:22:04 +0300

    Solved task 350

---
+++ Leetcode/Easy problems/350. Intersection of Two Arrays II.py
@@ -0,0 +1,72 @@
+"""
+350. Intersection of Two Arrays II
+Solved 05.06.2024
+Easy
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+ 
+
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+"""
+
+# Solution 1 (Time complexity O(k * (n + m)))
+from collections import Counter
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Time complexity O(k * (n + m))
+        result = []
+        unique_intersection = set(nums1) & set(nums2)
+        print(unique_intersection)
+
+        for i in unique_intersection:
+            result.extend([i] * min(nums1.count(i), nums2.count(i)))
+        
+        return result
+
+
+# Solution 2 (Two pointers and sorting, Time complexity O(nlogn))
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        result = []
+
+        i = 0
+        j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
+
+        return result
+        
