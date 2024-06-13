fef9018d5f8a41022cda9f8780796fa404a5ef3c
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-13 11:35:27 +0300

    Solved task 66

---
+++ Leetcode/Easy problems/66. Plus One.py
@@ -0,0 +1,84 @@
+"""
+66. Plus One
+Solved 13.06.2024
+Easy
+Topics: Array, Math
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. 
+The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+
+Example 3:
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+"""
+
+# Solution 1 (Brute force)
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = ''.join(list(map(str, digits)))
+        plus_one = str(int(number) + 1)
+        
+        return [int(i) for i in plus_one]
+
+
+# Solution 2 (Increase only one element in list)
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            elif digits[i] == 9:
+                digits[i] = 0
+            
+        return [1] + digits
+
+
+# Solution 3 (Recursion)
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        elif len(digits) == 1 and digits[0] == 9:
+            return [1, 0]
+        
+        return self.plusOne(digits[:-1]) + [0]
+    
