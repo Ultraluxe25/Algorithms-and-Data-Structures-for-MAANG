a6d408e82e334d9ff1eeca93179fcb4f537016bf
Alexander Kapturov <ultraluxe25@mail.ru>
2024-07-04 09:45:17 +0300

    Solved task 1822

---
+++ Leetcode/Easy problems/1822. Sign of the Product of an Array.py
@@ -0,0 +1,59 @@
+"""
+1822. Sign of the Product of an Array
+Solved 04.07.2024
+Easy
+Topics
+
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+
+Example 1:
+
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+
+
+Example 2:
+
+Input: nums = [1,5,0,2,-3]
+Output: 0
+Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+
+
+Example 3:
+
+Input: nums = [-1,1,-1,1,-1]
+Output: -1
+Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-100 <= nums[i] <= 100
+"""
+
+from functools import reduce
+
+class Solution:
+    def signFunc(self, product: int) -> int:
+        if product < 0:
+            return -1
+        elif product == 0:
+            return 0
+        return 1  # product > 0
+
+
+    def arraySign(self, nums: List[int]) -> int:
+        mult = reduce(lambda x, y: x * y, nums)
+        result = self.signFunc(mult)
+
+        return result
