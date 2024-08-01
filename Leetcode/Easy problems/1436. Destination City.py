fdaeb39f2ebd038315f5411ed8f89a8c4c9c6da4
Alexander Kapturov <ultraluxe25@mail.ru>
2024-08-01 20:09:38 +0300

    Solved task 1436

---
+++ Leetcode/Easy problems/1436. Destination City.py
@@ -0,0 +1,56 @@
+"""
+1436. Destination City
+Solved 01.08.2024
+Easy
+Topics: Array, Hash Table, String
+
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. 
+Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+
+Example 1:
+
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo" 
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+
+
+Example 2:
+
+Input: paths = [["B","C"],["D","B"],["C","A"]]
+Output: "A"
+Explanation: All possible trips are: 
+"D" -> "B" -> "C" -> "A". 
+"B" -> "C" -> "A". 
+"C" -> "A". 
+"A". 
+Clearly the destination city is "A".
+
+
+Example 3:
+
+Input: paths = [["A","Z"]]
+Output: "Z"
+
+Constraints:
+
+1 <= paths.length <= 100
+paths[i].length == 2
+1 <= cityAi.length, cityBi.length <= 10
+cityAi != cityBi
+All strings consist of lowercase and uppercase English letters and the space character.
+"""
+
+# Solution 1 (Hash Set)
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        cityA = set()
+        cityB = set()
+
+        for path in paths:
+            cityA.add(path[0])
+            cityB.add(path[1])
+
+        return tuple(cityB.difference(cityA))[0]
