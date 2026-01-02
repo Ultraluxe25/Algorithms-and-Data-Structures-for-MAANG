a6a96a684a8caa1176a20d2bc4d7d228af9950c8
Alexander Kapturov <ultraluxe25@mail.ru>
2026-01-02 15:10:13 +0300

    chore: add new version of task 66 solution

---
+++ Leetcode/2026/2026-66. Plus One.py
@@ -0,0 +1,67 @@
+"""
+66. Plus One
+Solved 01/01/2026
+Easy
+Topics: Array, Math
+
+You are given a large integer represented as an integer array digits, 
+where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. 
+The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Example 3:
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+"""
+
+# Beats 100% time O(n), 99.31% memory O(1).
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+
+        # Last digit is 9
+        digits[-1] = 0
+        n = len(digits)
+
+        for i in range(n-2, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            # Last digit is 9 again
+            digits[i] = 0
+        # All digits where 9 in a number
+        else:
+            digits.insert(0, 1)
+            return digits
